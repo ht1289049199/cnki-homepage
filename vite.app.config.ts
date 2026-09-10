121163699aa33147601d3c639d8069b1ea2374a6
贺霆TKD <115332004+ht1289049199@users.noreply.github.com>
2026-09-10 18:24:43 +0800

    add vite.app.config.ts

---
+++ vite.app.config.ts
@@ -0,0 +1,10 @@
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  build: { outDir: "dist-app" },
+});
